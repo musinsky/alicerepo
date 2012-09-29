876b916d20ca9f60db43403de49cc7ffb759a9d8
Martin Vala <Martin.Vala@cern.ch>
2012-09-29 22:47:33 +0400

    Downloading root tarball from root webpage instead of svn

--- utils/pull-root.sh
+++ utils/pull-root.sh
@@ -8,10 +8,11 @@ fi
 
 ROOT_VER=${1//-/.}
 
-SVN_PATH1="root_svn_$ROOT_VER"
-SVN_PATH2="root_v$ROOT_VER.source"
+wget ftp://root.cern.ch/root/root_v$ROOT_VER.source.tar.gz
 
-svn co https://root.cern.ch/svn/root/tags/v$1 $SVN_PATH1
-svn -q export $SVN_PATH1 $SVN_PATH2
-tar cfz $SVN_PATH2.tar.gz $SVN_PATH2
-rm -rf $SVN_PATH1
+#SVN_PATH1="root_svn_$ROOT_VER"
+#SVN_PATH2="root_v$ROOT_VER.source"
+#svn co https://root.cern.ch/svn/root/tags/v$1 $SVN_PATH1
+#svn -q export $SVN_PATH1 $SVN_PATH2
+#tar cfz $SVN_PATH2.tar.gz $SVN_PATH2
+#rm -rf $SVN_PATH1
